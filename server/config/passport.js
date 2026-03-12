25e32f9aeb25d3cd1a394b92261c89406fbc468a
shiwank <shiwank07@gmail.com>
2026-03-12 18:59:53 +0530

    Fix: update Google OAuth callback URL for production

--- server/config/passport.js
+++ server/config/passport.js
@@ -6,7 +6,7 @@ const User = require('../models/User');
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: '/api/auth/google/callback'
+    callbackURL: 'https://bite-hub-server.onrender.com/api/auth/google/callback'
   },
   async (accessToken, refreshToken, profile, done) => {
     try {
